14f570f12d1f7468465573e0cfabcd4c7c6bc92f
RehamWify <rehamwify@gmail.com>
2025-09-27 05:44:51 +0300

    fix use relative path

--- src/apis/allProducts.ts
+++ src/apis/allProducts.ts
@@ -2,7 +2,7 @@ import { Product } from "@/types/product.type";
 
 export default async function getAllProducts() {
 
-    const response = await fetch("http://localhost:3000/api/users");
+    const response = await fetch("/api/users");
     const { data } : {data: Product []} = await response.json();
 
     return data;
