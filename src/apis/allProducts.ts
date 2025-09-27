54a84a7a693de9195aaee59a45a4fbf5c44b8e16
RehamWify <rehamwify@gmail.com>
2025-09-27 06:16:32 +0300

    dynamic absolute URL

--- src/apis/allProducts.ts
+++ src/apis/allProducts.ts
@@ -1,9 +1,12 @@
 import { Product } from "@/types/product.type";
 
+// Get the base URL from the environment variables
+const BASE_URL = process.env.API;
+
 export default async function getAllProducts() {
-
-    const response = await fetch("/api/users");
-    const { data } : {data: Product []} = await response.json();
-
-    return data;
+  // ✅ CHANGE 2: Use the dynamic absolute URL
+  const response = await fetch(`${BASE_URL}/api/users`);
+  const { data } : {data: Product []} = await response.json();
+  
+  return data;
 }
