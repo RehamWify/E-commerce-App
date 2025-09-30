2247ca6d15dc26ef2c6ec89d5af3d1559d849a37
RehamWify <rehamwify@gmail.com>
2025-10-01 00:21:49 +0300

    add next configuration

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   images :{
     remotePatterns: [ new URL("https://ecommerce.routemisr.com/**/**")]
-  }
+  },
+  eslint :{
+    ignoreDuringBuilds: true,
+  },
+  typescript :{
+    ignoreBuildErrors : true,
+  },
 };
 
 export default nextConfig;
