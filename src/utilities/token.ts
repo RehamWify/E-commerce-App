3085901291c376dc7f0e2de7d4f4fa8af006fc14
RehamWify <rehamwify@gmail.com>
2025-10-11 18:14:00 +0300

    test token

--- src/utilities/token.ts
+++ src/utilities/token.ts
@@ -4,7 +4,7 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getMyToken() {
-  const x = (await cookies()).get("next-auth.session-token")?.value;
+  const x = (await cookies()).get("next-auth.session-token")?.value || (await cookies()).get("__Secure-next-auth.session-token")?.value
   
 
   const token = await decode({
